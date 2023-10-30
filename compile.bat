dc302f7c0a9076d155e3cba3b50f4327d0e79b07
Fernando Ribeiro Alves <fernandoribeiro889@gmail.com>
2023-10-30 04:11:31 -0300

    correções na importação e compilação, padronizado

--- compile.bat
+++ compile.bat
@@ -4,18 +4,17 @@ md .\build\deps .\build\bin .\build\logs
 
 @REM compilação da libs externas
 
-@REM compila glad como lib: -> gl.o
-@REM obs: alterei a importação no gl.c [<glad/gl.h>] para ["gl.h"]
-g++ -c -Iextern\glad\include\glad .\extern\glad\src\gl.c -o build\deps\gl.o
+@REM compila glad como lib: -> libgl.o
+g++ -c -Iextern\glad\include .\extern\glad\src\gl.c -o build\deps\libgl.o
 
 @REM compila glm\glm como lib: -> libglm.o
 g++ -c -Iextern\glm .\extern\glm\glm\detail\glm.cpp -o build\deps\libglm.o
 
-@REM copia a lib statica do glfw
+@REM copia a lib estatica do glfw
 copy .\extern\glfw\lib-static-ucrt\glfw3.dll build\bin\
 
 @REM compilação do source
 g++ -Wall -c .\src\main.cpp -Iextern\glfw\include -Iextern\glm -Iinclude -Iextern\glad\include -Iextern\stb\include -o build\deps\cube_test.o
-g++ -fPIC .\build\deps\cube_test.o .\build\deps\libglm.o .\build\deps\gl.o -Lextern\glfw\lib-static-ucrt -lglfw3dll -o build\bin\cube_test
+g++ -fPIC .\build\deps\cube_test.o .\build\deps\libglm.o .\build\deps\libgl.o -Lextern\glfw\lib-static-ucrt -lglfw3dll -o build\bin\cube_test
 
-echo "Compilação finalizada"
+echo "Compilação Finalizada."
